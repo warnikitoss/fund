d10badb8bdacad453c7ed6cebc163ffbd87e7595
warnikitoss <nikita44455@gmail.com>
2025-11-10 14:05:23 +0300

    Add files via upload

--- functions.c
+++ functions.c
@@ -1,32 +1,52 @@
-#include <string.h>
 #include "functions.h"
+#include <stdio.h>
 
-int check_brackets(const char *str) {
-    char stack[strlen(str)];
-    int j = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '(' || str[i] == '<' || str[i] == '{' || str[i] == '[') {
-            stack[j] = str[i];
-            j++;
-        } else if(str[i] == ')' || str[i] == '>' || str[i] == '}' || str[i] == ']') {
-            if (j == 0) {
-                return 0;
-            }
-            if (stack[j - 1] == '(' && str[i] == ')') {
-                j--;
-            } else if (stack[j - 1] == '<' && str[i] == '>') {
-                j--;
-            } else if (stack[j - 1] == '{' && str[i] == '}') {
-                j--;
-            } else if (stack[j - 1] == '[' && str[i] == ']') {
-                j--;
-            } else {
-                return 0;
-            }
+StatusCode decimal_to_base_2r(const unsigned int number, const int r, char **result) {
+    if (r < 1 || r > 5 || result == NULL) {
+        return ERROR_INVALID_INPUT;
+    }
+    const unsigned int base = 1 << r;
+    const unsigned int mask = base - 1;
+    
+    if (number == 0) {
+        *result = (char*)malloc(2);
+        if (*result == NULL) {
+            return ERROR_MEMORY_ALLOCATION;
         }
+        (*result)[0] = '0';
+        (*result)[1] = '\0';
+        return OK;
     }
-    if (j == 0) {
-        return 1;
+    
+    int length = 0;
+    unsigned int temp = number;
+    while (temp != 0) {
+        length++;
+        temp >>= r;
     }
-    return 0;
+    
+    *result = (char*)malloc(length + 1);
+    if (*result == NULL) {
+        return ERROR_MEMORY_ALLOCATION;
+    }
+    
+    temp = number;
+    int pos = length;
+    (*result)[pos] = '\0';
+    pos--;
+    
+    while (temp != 0) {
+        unsigned int digit = temp & mask;
+        
+        if (digit < 10) {
+            (*result)[pos] = '0' + digit;
+        } else {
+            (*result)[pos] = 'A' + (digit - 10);
+        }
+        
+        temp >>= r;
+        pos--;
+    }
+    
+    return OK;
 }
--- functions.h
+++ functions.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-int check_brackets(const char *str);
+#include <stdlib.h>
+
+typedef enum {
+    OK = 0,
+    ERROR_INVALID_INPUT,
+    ERROR_MEMORY_ALLOCATION
+} StatusCode;
+
+StatusCode decimal_to_base_2r(const unsigned int number, const int r, char **result);
 
 #endif
